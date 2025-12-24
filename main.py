4af7c5eef8a72e4bd8d0aab33848618cb238058b
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-12-23 19:56:10 -0500

    refactored for multi server

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 import discord
 from ros_errors import ROSErrors as ros
+import os
+import yaml
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+token = os.environ.get('DISCORD_BOT_KEY')
 
-try:
-    with open('.discord_bot_key', 'r') as f:
-        token = f.readline().strip()  # discord API key
-        channel_id = int(f.readline().strip())  # channel ID
-except FileNotFoundError:
-    print("file not found")
-    exit(1)
+class errorBot:
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
 
-except Exception as e:
-    print(f"Needed information is not there {e}")
-    exit(1)
+        try:
+            with open('config.yaml', 'r') as f:
+                config = yaml.safe_load(f)
+        except FileNotFoundError:
+            print('config not found')
+            exit(1)
+        
+        self.channel = config['channels']['bot_test']
+        
+    @client.event
+    async def on_ready():
+        print(f'logged in as {client.user}')
 
-def bot_test(channel_id):
-    if channel_id == channel_id:
-        return True
-    else:
-        return False
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-@client.event
-async def on_ready():
-    print(f'logged in as {client.user}')
+        # respond with a ros error
+        if message.content.lower() == 'ros': 
+            ros_msg = ros.get_error_msg()
+            await message.channel.send(f"`{ros_msg}`")
+            print(f'sent ros error to: {message.channel.id}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+        # be mad at a Noetic reference
+        elif message.content.lower() == 'rosrun':
+            await message.channel.send('AHHHHHHH ROS1 Reference!')
+            print(f'Noetic reference detected in channel: {message.channel.id}')
 
-    # respond with a ros error
-    if message.content.lower() == 'ros' and bot_test(message.channel.id): 
-        ros_msg = ros.get_error_msg()
-        await message.channel.send(f"`{ros_msg}`")
-
-    elif message.content.lower() == 'rosrun' and bot_test(message.channel.id):
-        await message.channel.send('AHHHHHHH ROS1 Reference!')
-
-client.run(token) # use token
+if __name__ == '__main__':
+    client.run(token) 
